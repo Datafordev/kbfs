1174761188a3a09e112795bae005521e06d57fe5
Steve Sanders <steve@keyba.se>
2016-10-28 15:46:08 -0700

    Use service RPC to get mount drive letter

--- libdokan/start.go
+++ libdokan/start.go
@@ -61,8 +61,16 @@ func Start(mounter Mounter, options StartOptions, kbCtx libkbfs.Context) *libfs.
 	if err != nil {
 		return libfs.InitError(err.Error())
 	}
+
 	options.DokanConfig.FileSystem = fs
 	options.DokanConfig.Path = mounter.Dir()
+	if options.DokanConfig.Path == "" {
+		// The mounter will detect this case and pick up the path from DokanConfig
+		options.DokanConfig.Path, err = config.KeybaseService().GetMountDir(ctx)
+		if err != nil {
+			return libfs.InitError(err.Error())
+		}
+	}
 
 	if newFolderNameErr != nil {
 		log.CWarningf(ctx, "Error guessing new folder name: %v", newFolderNameErr)
